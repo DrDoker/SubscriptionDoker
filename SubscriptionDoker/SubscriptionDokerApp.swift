2f14138a2b6b6163fdc2866603d3f06948256a09
Serhii Tkachenko <doker.tk@gmail.com>
2024-10-02 21:34:43 +0200

    Initial Commit

---
+++ SubscriptionDoker/SubscriptionDokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionDokerApp.swift
+//  SubscriptionDoker
+//
+//  Created by Serhii on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionDokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
